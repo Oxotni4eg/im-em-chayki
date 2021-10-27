dd46f85eb1a9e019bf95aecb5dd9e312b7415629
Vlad Shpilevskii <vlad.szma@gmail.com>
2021-10-27 21:14:12 +0300

    add send form to e-mail

--- app/Http/Controllers/Ajax/ContactController.php
+++ app/Http/Controllers/Ajax/ContactController.php
@@ -4,11 +4,27 @@ namespace App\Http\Controllers\Ajax;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Mail;
 
 class ContactController extends Controller
 {
     public function send(Request $request)
     {
-        dd($request->all());
+        $result = false;
+
+        if($request->ajax() && !empty($request->all()))
+        {
+            $sender = $request;
+            var_dump($sender);
+            Mail::send('emails.default', ['sender' => $sender], function($message) use ($sender) {
+                $message->from($sender->InputEmail, $sender->InputSender);
+                $message->to(env('MAIL_ADMIN_EMAIL'));
+                $message->subject('Новое сообщение от клиента !');
+            });
+
+            $result = true;
+        }
+
+        return response()->json(['result' => $result]);
     }
 }
